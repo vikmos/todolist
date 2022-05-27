5487e30fdcd58dcb47a5074a39ffe127e6c2d3b5
Viktor Moskalenko <tehb23@gmial.com>
2022-05-27 14:26:01 +0300

    TODOList app, start

---
+++ todolist/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Task(models.Model):
+    """Класс для заданий"""
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length=400)
+    create_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    def create_task(self):
+        self.create_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
