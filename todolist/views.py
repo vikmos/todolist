567ea45794b8ced51bdd2ecdceb7d7d9341fb561
Viktor Moskalenko <tehb23@gmial.com>
2022-05-28 13:31:12 +0300

    Modified templates to display task from databse

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.filter(create_date__lte=timezone.now()).order_by('create_date')
+    return render(request, 'todolist/task_list.html', {'tasks': tasks})
