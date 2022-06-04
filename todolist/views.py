1ccc586c57dc502d4e18cacb22a46b5dae09b93f
Viktor Moskalenko <tehb23@gmial.com>
2022-06-04 20:36:41 +0300

    Some style added

--- todolist/views.py
+++ todolist/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import Task
 
 def task_list(request):
-    tasks = Task.objects.filter(create_date__lte=timezone.now(), task_status=False).order_by('create_date')
+    tasks = Task.objects.filter(create_date__lte=timezone.now()).order_by('create_date')
     return render(request, 'todolist/task_list.html', {'tasks': tasks})
