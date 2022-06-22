3959661458ffee3c44551d3b0060545c015b3b36
Viktor Moskalenko <tehb23@gmial.com>
2022-06-22 19:16:44 +0300

    Передаём на фронт, два списка заданий

--- todolist/views.py
+++ todolist/views.py
@@ -3,5 +3,9 @@ from django.utils import timezone
 from .models import Task
 
 def task_list(request):
-    tasks = Task.objects.filter(create_date__lte=timezone.now()).order_by('create_date')
+    current_tasks = Task.objects.filter(create_date__lte=timezone.now()).\
+            exclude(task_status=False).order_by('create_date')
+    end_tasks = Task.objects.filter(create_date__lte=timezone.now()).\
+            exclude(task_status=True).order_by('create_date')
+    tasks = {'current_tasks': current_tasks, 'end_tasks': end_tasks}
     return render(request, 'todolist/task_list.html', {'tasks': tasks})
