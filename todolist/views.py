a6d5e89831a9e4127b6ac3d30240d23413eddb32
Viktor Moskalenko <tehb23@gmail.com>
2022-06-22 22:51:44 +0300

    Current/End tasks styled

--- todolist/views.py
+++ todolist/views.py
@@ -4,8 +4,8 @@ from .models import Task
 
 def task_list(request):
     current_tasks = Task.objects.filter(create_date__lte=timezone.now()).\
-            exclude(task_status=False).order_by('create_date')
+    exclude(task_status=True).order_by('create_date')
     end_tasks = Task.objects.filter(create_date__lte=timezone.now()).\
-            exclude(task_status=True).order_by('create_date')
+    exclude(task_status=False).order_by('create_date')
     tasks = {'current_tasks': current_tasks, 'end_tasks': end_tasks}
     return render(request, 'todolist/task_list.html', {'tasks': tasks})
