9d2d8dd94af8e401127c050475dc5fa5efd74d2b
Viktor Moskalenko <tehb23@gmail.com>
2022-07-06 18:28:16 +0300

    Add new task completed

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         path('', views.task_list, name='task_list'),
+        path('task/new/', views.task_new, name='task_new'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
     current_tasks = Task.objects.filter(create_date__lte=timezone.now()).\
@@ -9,3 +11,17 @@ def task_list(request):
     exclude(task_status=False).order_by('create_date')
     tasks = {'current_tasks': current_tasks, 'end_tasks': end_tasks}
     return render(request, 'todolist/task_list.html', {'tasks': tasks})
+
+def task_new(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author =request.user
+            task.creat_date = timezone.now()
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+    return render(request, 'todolist/task_new.html', {'form': form})
+
