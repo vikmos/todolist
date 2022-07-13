68f1e7d5302f7ea2042fc747655cf929e2bffc53
Viktor Moskalenko <tehb23@gmail.com>
2022-07-13 19:17:13 +0300

    Изменена форма добавления нового задания

--- todolist/forms.py
+++ todolist/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
-
+    text = forms.CharField(label='Введите новое задание:',
+            widget = forms.widgets.TextInput(attrs={'size': '100'}))
     class Meta:
         model = Task
         fields = ('text',)
+
