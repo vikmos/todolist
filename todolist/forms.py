95e3ef48328170f1638ac4866ed73f95969445ec
Viktor Moskalenko <tehb23@gmail.com>
2022-07-20 21:31:34 +0300

    Class meta widgets added

--- todolist/forms.py
+++ todolist/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
-    text = forms.CharField(label='Введите новое задание:',
-            widget = forms.widgets.TextInput(attrs={'size': '100'}))
     class Meta:
         model = Task
         fields = ('text',)
-
+        widgets = {
+                'text': forms.TextInput(attrs={'size': '50'})
+                }
